2827660ae22c2641d6ca11e6059e0b0594b5be1e
Alsch <alex@schwalbemn.com>
2019-08-11 18:52:19 -0500

    Initial src commit

---
+++ COMENUM/COMENUM/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMENUM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            string port_list = "";
+            
+            foreach (string port in ports)
+            {
+                port_list += (port + "\n");
+            }
+
+            var notification = new System.Windows.Forms.NotifyIcon()
+            {
+                Visible = true,
+                Icon = System.Drawing.SystemIcons.Information,
+                BalloonTipIcon = ToolTipIcon.None, // Setting this property results in an ugly GUID appearing at the bottom of the balloontip 
+                BalloonTipTitle = "Serial Ports Found:",
+                BalloonTipText = port_list,                
+            };
+
+
+            notification.ShowBalloonTip(10000);
+
+            notification.Dispose();
+        } 
+    }
+}
